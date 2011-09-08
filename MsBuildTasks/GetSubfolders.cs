f671ac9263f031e3c35069ce34ca503e96aa1c5b
Steve Sanderson <steve@codeville.net>
2011-09-08 10:03:59 +0100

    Improve build to find site templates automatically Also now actually works when installed into WebMatrix (removes special chars from template names)

---
+++ MsBuildTasks/GetSubfolders.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace WebMatrixSiteTemplates.MsBuildTasks
+{
+    public class GetSubfolders : ITask
+    {
+        public IBuildEngine BuildEngine { get; set; }
+        public ITaskHost HostObject { get; set; }
+        [Required] public string FolderPath { get; set; }
+        [Output] public ITaskItem[] Results { get; set; }
+
+        public bool Execute() {
+            var subfolders = Directory.GetDirectories(FolderPath);
+            Results = subfolders.Select(x => new TaskItem(Path.GetFileName(x))).ToArray();
+            return true;
+        }
+    }
+}
