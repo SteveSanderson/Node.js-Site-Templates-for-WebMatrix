67236afbf4111e0bbc160b51cf5b14b44214d1a4
Steve Sanderson <steve@codeville.net>
2011-09-08 14:25:52 +0100

    Fixes for UAC compatibility Now checks for WebMatrix presence before allowing install

--- WixCustomActions/CustomAction.cs
+++ WixCustomActions/CustomAction.cs
@@ -1,8 +1,10 @@
 ﻿using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Security.Principal;
 using System.Xml.Linq;
 using Microsoft.Deployment.WindowsInstaller;
+using System;
 
 namespace WebMatrixSiteTemplates.WixCustomActions
 {
@@ -12,6 +14,14 @@ namespace WebMatrixSiteTemplates.WixCustomActions
         private const string OwnedFeedEntriesIdentifier = "{58520BA8-DF03-4D4B-A311-5452F5DA5957}";
         private static readonly XNamespace FeedEntriesNamespace = XNamespace.Get("http://www.w3.org/2005/Atom");
 
+        private static string TemplateFeedEntriesTempFolder {
+            get { return Path.Combine(Path.GetTempPath(), "TemplateFeedEntries"); }
+        }
+
+        private static string WebMatrixTemplatesFolder {
+            get { return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "Microsoft WebMatrix", "templates"); }
+        }
+
         [CustomAction]
         public static ActionResult RegisterTemplateFeedItems(Session session) {
             // First clear out any previous versions
@@ -20,14 +30,15 @@ namespace WebMatrixSiteTemplates.WixCustomActions
                 return removeTemplateFeedItemsResult;
 
             // ... then add the new ones
-            var feedEntriesDir = session.GetTargetPath("TemplateFeedEntries");
+            var feedEntriesDir = TemplateFeedEntriesTempFolder;
             if (Directory.Exists(feedEntriesDir)) {
 
                 var feedEntries = Directory.EnumerateFiles(feedEntriesDir, "*.xml");
-                var webMatrixFeedFile = Path.Combine(session.GetTargetPath("WebMatrixTemplates"), "TemplateFeed.xml");
-
-                foreach (var feedEntryPath in feedEntries) {
-                    RegisterFeedEntries(webMatrixFeedFile, feedEntryPath);
+                var webMatrixFeedFile = Path.Combine(WebMatrixTemplatesFolder, "TemplateFeed.xml");
+                if (File.Exists(webMatrixFeedFile)) {
+                    foreach (var feedEntryPath in feedEntries) {
+                        RegisterFeedEntries(webMatrixFeedFile, feedEntryPath);
+                    }
                 }
             }
             return ActionResult.Success;
@@ -46,21 +57,19 @@ namespace WebMatrixSiteTemplates.WixCustomActions
         }
 
         private static ActionResult RemoveTemplateFeedItems(Session session) {
-            var webMatrixFeedFile = Path.Combine(session.GetTargetPath("WebMatrixTemplates"), "TemplateFeed.xml");
+            var webMatrixFeedFile = Path.Combine(WebMatrixTemplatesFolder, "TemplateFeed.xml");
             if (File.Exists(webMatrixFeedFile)) {
                 var feedXml = XDocument.Load(webMatrixFeedFile);
                 var oldItems = from node in feedXml.Root.Elements(FeedEntriesNamespace + "entry")
                                let installedByNode = node.Element(FeedEntriesNamespace + "installedBy")
                                where installedByNode != null && installedByNode.Value == OwnedFeedEntriesIdentifier
                                select node;
-                foreach(var itemToRemove in oldItems.ToList())
+                foreach (var itemToRemove in oldItems.ToList())
                     itemToRemove.Remove();
                 feedXml.Save(webMatrixFeedFile);
-
-                return ActionResult.Success;    
             }
 
-            return ActionResult.Failure;
+            return ActionResult.Success;
         }
     }
 }
