8dec7fc09821d5fddc9029e21523117c190bada5
Steve Sanderson <steve@codeville.net>
2011-09-15 11:39:09 +0100

    Changed empty template to return content type "text/html", and bumped version to 0.0.3

--- Sites/NodeJsSite/app.js
+++ Sites/NodeJsSite/app.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end('Hello, world!');
     
 }).listen(process.env.PORT);
